4b2714224cbf3e67e9216847d11eb2c8da158017
Alexandre Almeida <alee.wxa@gmail.com>
2022-07-04 10:42:34 -0300

    feat: adiciona last_name, cpf, is_admin, phone, migration user

--- projeto-empresarial/database/migrations/2014_10_12_000000_create_users_table.php
+++ projeto-empresarial/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,23 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
+            $table->string('cpf')->unique();
+            $table->string('phone')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('is_admin')->default(false);;
         });
     }
 
@@ -29,8 +31,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('users');
     }
 };
