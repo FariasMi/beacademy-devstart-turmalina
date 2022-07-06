34f952af73cc76b142afac99c7e234c14fb16795
Alexandre Almeida <alee.wxa@gmail.com>
2022-07-06 16:56:53 -0300

    feature: adiciona escolha tipo de usuário

--- projeto-empresarial/resources/views/admin/create.blade.php
+++ projeto-empresarial/resources/views/admin/create.blade.php
@@ -59,7 +59,15 @@
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
+                <div class="flex items-center mr-4">
+                    <input id="administrator" type="radio" value="admin" name="type_user" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    <label for="administrator" class="ml-2 text-sm font-medium text-gray-700">Aministrador</label>
+                </div>
+                <div class="flex items-center mr-4">
+                    <input checked id="user" type="radio" value="user" name="type_user" class="w-4 h-4 btn-radio">
+                    <label for="user" class="ml-2 text-sm font-medium text-gray-700 ">Usuário comum</label>
+                </div>
                 <x-button class="ml-4">
                     {{ __('Cadastrar Usuário') }}
                 </x-button>
