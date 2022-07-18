a138f414c946b67f01ee07d8d05bf9178e02e688
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-07-18 15:53:44 -0300

    feat: carrossel funcionalidades e styles de cards

--- projeto-empresarial/resources/views/template/default.blade.php
+++ projeto-empresarial/resources/views/template/default.blade.php
@@ -1,42 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield("title")</title>
-
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+    <title>@yield("title")</title>
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/carroussel.css', 'resources/js/app.js', 'resources/js/carroussel.js'])
 </head>
-
-<body class="font-sans antialiased">
-    <div class=" bg-gray-100">
-        @include('layouts.navigation')
-
-        <!-- Page Heading -->
-        <header class="bg-white shadow">
-                @yield("header")
-        </header>
-
-        <!-- Page Content -->
-        <main>
-            @yield("main")
-        </main>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight hover:text-indigo-400 cursor-pointer">
-                {{ __('Lista de Usuários') }}
-            </h2>
-        </x-slot>
-
-        <footer class="flex justify-center p-8 mt-8  border-2">
-            <p> &copy;2022 turmalina todos os direitos reservados  </p>
-        </footer>
-    </div>
-</body>
-
+    <body class="font-sans antialiased">
+        <div class=" bg-gray-100">
+            @include('layouts.navigation')
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                    @yield("header")
+            </header>
+            <!-- Page Content -->
+            <main>
+                @yield("main")
+            </main>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight hover:text-indigo-400 cursor-pointer">
+                    {{ __('Lista de Usuários') }}
+                </h2>
+            </x-slot>
+            <footer class="flex justify-center p-8 mt-8  border-2">
+                <p> &copy;2022 turmalina todos os direitos reservados  </p>
+            </footer>
+        </div>
+        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    </body>
 </html>
 
