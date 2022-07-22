308652f314b66a1cfb6c0539f1ab8f1dc16cf281
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-07-21 23:27:44 -0300

    chore: remove methods nao usados

--- projeto-empresarial/resources/views/index.blade.php
+++ projeto-empresarial/resources/views/index.blade.php
@@ -42,7 +42,7 @@
                     <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
                 </div>
                 <div class="flex justify-between items-center">
-                    <span class="text-3xl font-bold text-gray-900 dark:text-black">$599</span>
+                    <span class="text-3xl font-bold text-gray-900 dark:text-black">R$ 180</span>
                     <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">visualizar</a>
                 </div>
             </div>
@@ -65,7 +65,7 @@
                     <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
                 </div>
                 <div class="flex justify-between items-center">
-                    <span class="text-3xl font-bold text-gray-900 dark:text-black">$599</span>
+                    <span class="text-3xl font-bold text-gray-900 dark:text-black">R$ 159</span>
                     <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">visualizar</a>
                 </div>
             </div>
@@ -88,7 +88,7 @@
                     <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
                 </div>
                 <div class="flex justify-between items-center">
-                    <span class="text-3xl font-bold text-gray-900 dark:text-black">$599</span>
+                    <span class="text-3xl font-bold text-gray-900 dark:text-black">R$ 74</span>
                     <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">visualizar</a>
                 </div>
             </div>
