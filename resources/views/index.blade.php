a150d84c37c484891aec85a4dec41cbc8b223fcf
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-07-27 21:07:08 -0300

    fix: preço produto no home e migration orders

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,7 +43,7 @@
                         <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
                     </div>
                     <div class="flex justify-between items-center">
-                        <span class="text-3xl font-bold text-gray-900 dark:text-black ">R$ {{ $product->price }}</span>
+                        <span class="text-3xl font-bold text-gray-900 dark:text-black ">R$ {{ $product->sale_price }}</span>
 
                     </div>
                 </div>
