55fb4db424816cce9c722feeb383f8c9c8c1eaad
Alexandre Almeida <alee.wxa@gmail.com>
2022-08-02 19:18:27 -0300

    feat: Update Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,19 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'John',
-            'last_name' => 'Doe',
-            'cpf' => "38406720077",
-            'phone' => "00000000000",
-            'date_of_birth' => '2000-01-01',
-            'email' => 'john.doe@email.com',
-            'password' => bcrypt('12345678'),
-            'is_admin' => 1,
+        $this->call([
+            UserTableSeeder::class,
+            ProductSeeder::class
         ]);
-
-        $this->call(ProductSeeder::class);
     }
 }
