9cbfd3fcf29a925e70d7f601c773ffb9e79d4cb4
Alexandre Almeida <alee.wxa@gmail.com>
2022-08-02 10:01:37 -0300

    feat: botão cadastre-se login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,6 +48,12 @@
                     {{ __('Entrar') }}
                 </x-button>
             </div>
+            <div class="flex justify-end mt-4 items-center">
+                <p class="text-sm text-gray-600">Ainda não é cadastrado? 😮</p>
+                <a href="{{ route('register') }}" class="ml-3 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                    Cadastre-se
+                </a>
+            </div>
         </form>
     </x-auth-card>
 </x-guest-layout>
