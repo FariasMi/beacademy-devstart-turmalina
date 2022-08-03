b9b70bfb3a34dfd000a1c5b7e633ab8dfaef02c9
Alexandre Almeida <alee.wxa@gmail.com>
2022-08-02 21:59:42 -0300

    fix: variável produts na página de usuários

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -78,7 +78,7 @@
         </tbody>
     </table>
     <div class="w-full grid justify-items-center mt-8">
-        {{ $products->appends(Request::except('page'))->links('vendor.pagination.tailwind') }}
+        {{ $users->appends(Request::except('page'))->links('vendor.pagination.tailwind') }}
     </div>
 </div>
 @endsection
