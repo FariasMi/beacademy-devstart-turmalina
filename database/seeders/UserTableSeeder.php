aa7a37fa17b33ade48267269d781b51ef5d2c839
Alexandre Almeida <alee.wxa@gmail.com>
2022-08-04 09:05:31 -0300

    feat: Remove Factory Seeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -43,7 +43,5 @@ class UserTableSeeder extends Seeder
                 'is_admin' => 0,
                 ]);      
         }
-            
-        \App\Models\User::factory(10)->create();
     }
 }
